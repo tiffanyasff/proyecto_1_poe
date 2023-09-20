028b902bcb8b3dc9ca0870b0dcfa6e5744995991
pablo b <pabloestebanbecerra@gmail.com>
2023-09-20 18:00:58 -0500

    c6

---
+++ Proyeccto_1/src/main/java/Proyeccto_1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author pablo
+ */
+public class Proyeccto_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
