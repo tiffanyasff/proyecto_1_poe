4d64c71b0b1572849554692c69425290d1e115cc
pablo b <pabloestebanbecerra@gmail.com>
2023-09-20 17:03:22 -0500

    creacion del proyecto

---
+++ proyecto_1/src/main/java/com/lcaps/proyecto_1/Proyecto_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lcaps.proyecto_1;
+
+/**
+ *
+ * @author pablo
+ */
+public class Proyecto_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
