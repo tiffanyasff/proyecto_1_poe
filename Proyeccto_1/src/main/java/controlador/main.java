0b2d78e08df992fdef4657f22ecff5abe609a026
Pablo Becerra <pabloestebanbecerra@gmail.com>
2023-09-20 22:32:30 -0500

    arganizacion

--- Proyeccto_1/src/main/java/controlador/main.java
+++ Proyeccto_1/src/main/java/controlador/main.java
@@ -1,14 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package controlador;
 
 /**
  *
  * @author pablo
  */
-public class Proyeccto_1 {
-
+public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
 }
---
+++ Proyeccto_1/src/main/java/modelo/Figura.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author pablo
+ */
+public class Figura {
+    String nombre;
+    String tamano;
+
+    public Figura(String nombre, String tamano) {
+        this.nombre = nombre;
+        this.tamano = tamano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
