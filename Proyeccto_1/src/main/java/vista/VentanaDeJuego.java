0ad09c2b87d62c425717bac8b944399c26635eac
pablo b <pabloestebanbecerra@gmail.com>
2023-09-26 12:16:33 -0500

    ventanaDelJuego

--- Proyeccto_1/src/main/java/vista/VentanaDeJuego.java
+++ Proyeccto_1/src/main/java/vista/VentanaDeJuego.java
@@ -10,13 +10,23 @@ package vista;
  */
 import javax.swing.*;
 import java.awt.*;
+import logica.JuegoAdivinarFig;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
-public class VentanaDeJuego {
+
+public class VentanaDeJuego extends JFrame implements ActionListener {
+    private JuegoAdivinarFig juego; 
     private JFrame ventanaJuego;
     private JButton btnUno;
     private JButton btnDos;
     private JButton  btnTres;
     private JButton   btnCuatro;
+    private ImageIcon im;
+    private Icon i ;
     
     public VentanaDeJuego(){
         ventanaJuego = new JFrame("Juego");
@@ -26,28 +36,63 @@ public class VentanaDeJuego {
     }
     
     public void iniciarComponentes(){
+        juego = new JuegoAdivinarFig();
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(1, 4));
 
-        // Etiqueta y campo de texto para ingresar el nombre 
+        btnUno.setBounds(10, 10, 10, 10);
+        im = new ImageIcon("circulo.png");
+        i = new ImageIcon(im.getImage().getScaledInstance(btnUno.getWidth(), btnUno.getHeight(),Image.SCALE_DEFAULT));
+        btnDos.setIcon(i);
         
         btnUno = new JButton("boton uno");
         panel.add(btnUno);
         btnUno.setEnabled(false);
         
-        btnDos = new JButton("boton dos");
+        btnDos = new JButton(im);
         panel.add(btnDos);
-        
         
         
         btnTres = new JButton("boton tres");
         panel.add( btnTres);
         
-         btnCuatro = new JButton("boton tres");
+        btnCuatro = new JButton("boton tres");
         panel.add( btnCuatro);
         
         
+        //btnUno.addActionListener(this);
+        //btnDos.addActionListener(this);
+        //btnTres.addActionListener(this);
+        //btnCuatro.addActionListener(this);
+        
         ventanaJuego.add(panel);
         ventanaJuego.setVisible(true);
+        
+        //btnUno.setIcon(cargarImagen("src/img/circulo.png"));
+        //btnDos.setIcon(cargarImagen("src/img/circulo.png"));
+        //btnTres.setIcon(cargarImagen("src/img/circulo.png"));
+        //btnCuatro.setIcon(cargarImagen("src/img/circulo.png"));
+        
+        
+    }
+    
+    private void asignarValoresAleatorios() {
+        Random random = new Random();
+        btnUno.setText(String.valueOf(random.nextInt(3) + 1));
+        btnDos.setText(String.valueOf(random.nextInt(3) + 1));
+        btnTres.setText(String.valueOf(random.nextInt(3) + 1));
+        btnCuatro.setText(String.valueOf(random.nextInt(3) + 1));
+    }
+    
+    private ImageIcon cargarImagen(String nombreArchivo) {
+        return new ImageIcon(getClass().getResource(nombreArchivo));
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton botonPresionado = (JButton) e.getSource();
+
+        
+        asignarValoresAleatorios();
     }
 }
