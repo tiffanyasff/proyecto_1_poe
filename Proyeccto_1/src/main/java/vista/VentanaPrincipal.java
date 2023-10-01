cf6316e04c3dff24cc488c8e2a971e1a1607facb
pablo b <pabloestebanbecerra@gmail.com>
2023-10-01 11:36:09 -0500

    20

--- Proyeccto_1/src/main/java/vista/VentanaPrincipal.java
+++ Proyeccto_1/src/main/java/vista/VentanaPrincipal.java
@@ -12,14 +12,22 @@ package vista;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaPrincipal {
     private JFrame ventana;
     private JTextField campoNombre;
     private JButton btnIniciarJuego;
     private JButton btnInstrucciones;
-    private JLabel etqNombre;   
+    private JLabel etqNombre; 
+    private JLabel etqTitulo; 
     private JPanel panel;
+    private Color[] rainbowColors = {
+            Color.RED, Color.ORANGE, Color.YELLOW,
+            Color.GREEN, Color.BLUE, Color.MAGENTA
+    };
+    private int currentColorIndex = 0;
     
     
     
@@ -33,13 +41,34 @@ public class VentanaPrincipal {
     public void iniciarComponentes(){
         panel = new JPanel();
         panel.setLayout(null);
-        
-        
+
+        Color fondoAzulClaro = new Color(173, 216, 230);
+        panel.setBackground(fondoAzulClaro);
+
+        Font comicSansFont = new Font("Comic Sans MS", Font.PLAIN, 28);
+
         etqNombre = new JLabel("Nombre del jugador: ");
-        etqNombre.setBounds(50, 50, 280, 200);
+        etqNombre.setBounds(250, -20, 280, 200);
+        etqNombre.setFont(comicSansFont);
         panel.add(etqNombre);
+
+        etqTitulo = new JLabel("Pon a prueba tu capacidad visual ");
+        etqTitulo.setBounds(150, -60, 280, 200);
+        etqTitulo.setFont(comicSansFont);
+        panel.add(etqTitulo);
         
+        Timer rainbowTimer = new Timer(500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                etqTitulo.setForeground(rainbowColors[currentColorIndex]);
+                currentColorIndex = (currentColorIndex + 1) % rainbowColors.length;
+            }
+        });
+        rainbowTimer.start();
+
         ventana.add(panel);
         ventana.setVisible(true);
     }
 }
+
+
